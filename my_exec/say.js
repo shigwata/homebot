ff76c4e41e6f93647954d98774a36f20df61229b
shigwata <shigwata01@gmail.com>
2018-02-11 02:38:54 +0900

    add say

---
+++ my_exec/say.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+const home = require('google-home-notifier');
+const argv = process.argv;
+const text = argv[2] || '';
+const lang = argv[3] || 'ja';
+const device = argv[4] || '';
+
+home.device(device, lang);
+home.notify(text, (res) => console.log('said ' + text));
